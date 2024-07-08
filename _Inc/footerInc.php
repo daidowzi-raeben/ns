9761adf30d060fbc1e65b108c8dbd69845bf32b9
장영욱 <raeben@naver.com>
2024-07-08 17:08:12 +0900

    fix

--- _Inc/footerInc.php
+++ _Inc/footerInc.php
@@ -1,8 +1,8 @@
 <footer class="div-wrap footer-wrap" id="footer-wrap">
-  <div id="footer" class="div-cont">
-  <div id="f-logo"><span class="blind">NS홈쇼핑</span></div>
-  <div id="f-info">
-    <p>경기도 성남시 분당구 판교로 228번길 15  &nbsp;&nbsp;&nbsp;&nbsp;(문의) 준법지원팀 : 02-6336-1875, 감사실 : 02-6336-1272</p>
-    <p>COPYRIGHT © 2019 BY NS SHOPPING. CO., LTD. ALL RIGHTS RESERVED </p>
-  </div>
+    <div id="footer" class="div-cont">
+        <div id="f-logo"><span class="blind">NS홈쇼핑</span></div>
+        <div id="f-info">
+            <p>경기도 성남시 분당구 판교로 228번길 15 </p>
+            <p>COPYRIGHT © 2019 BY NS SHOPPING. CO., LTD. ALL RIGHTS RESERVED </p>
+        </div>
 </footer>
