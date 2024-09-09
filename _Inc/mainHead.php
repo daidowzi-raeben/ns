3ba6d91ece88f09ee90dd86deec315af8e00ef94
장영욱 <raeben@naver.com>
2024-09-09 12:51:18 +0900

    fix

--- _Inc/mainHead.php
+++ _Inc/mainHead.php
@@ -1,50 +1,49 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang ='ko'>
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang='ko'>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0 ,maximum-scale=1.0, minimum-scale=1.0,user-scalable=no,target-densitydpi=medium-dpi"> 반응형용 -->
-<title>NS홈쇼핑</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0 ,maximum-scale=1.0, minimum-scale=1.0,user-scalable=no,target-densitydpi=medium-dpi"> 반응형용 -->
+    <title>NS홈쇼핑</title>
 
-	<link type="text/css" rel="stylesheet" media="all" href=" _Css/common.css" />
-	<link type="text/css" rel="stylesheet" media="all" href=" _Css/styleDefault.css" />
-	<link type="text/css" rel="stylesheet" media="all" href=" _Css/layout.css" />
-	<link type="text/css" rel="stylesheet" media="all" href=" _Css/main.css" />
-	
-	<link rel="stylesheet" href=" _Js/jquery/slick-theme.css">
-	<link rel="stylesheet" href=" _Js/jquery/slick.css">
-	<script  type="text/javascript" src=" _Js/jquery/jquery-1.11.1.min.js"></script>
-    <script  type="text/javascript" src=" _Js/jquery/jquery-1.11.3.min.js"></script>
-	<script  type="text/javascript" src=" _Js/jquery/jquery.easing.1.3.js"></script>
-	<script  type="text/javascript" src=" _Js/jquery/slick.js"></script>
-	<script  type="text/javascript" src=" _Js/topmenu.js"></script>
-	<script  type="text/javascript" src=" _Js/defalut.js"></script>
-	<script  type="text/javascript" src=" _Js/common.js"></script>
-	<script  type="text/javascript" src=" _Js/main.js"></script>
-<meta http-equiv="X-UA-Compatible" content="IE=8">
+    <link type="text/css" rel="stylesheet" media="all" href=" _Css/common.css?v=<?php echo date("his")?>" />
+    <link type="text/css" rel="stylesheet" media="all" href=" _Css/styleDefault.css?v=<?php echo date("his")?>" />
+    <link type="text/css" rel="stylesheet" media="all" href=" _Css/layout.css?v=<?php echo date("his")?>" />
+    <link type="text/css" rel="stylesheet" media="all" href=" _Css/main.css?v=<?php echo date("his")?>" />
 
-<!--[if lt IE 8]>
+    <link rel="stylesheet" href=" _Js/jquery/slick-theme.css">
+    <link rel="stylesheet" href=" _Js/jquery/slick.css">
+    <script type="text/javascript" src=" _Js/jquery/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src=" _Js/jquery/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src=" _Js/jquery/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src=" _Js/jquery/slick.js"></script>
+    <script type="text/javascript" src=" _Js/topmenu.js"></script>
+    <script type="text/javascript" src=" _Js/defalut.js"></script>
+    <script type="text/javascript" src=" _Js/common.js"></script>
+    <script type="text/javascript" src=" _Js/main.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=8">
+
+    <!--[if lt IE 8]>
 <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE8.js"></script>
-<![endif]-->    
+<![endif]-->
 </head>
+
 <body id="body">
-<div id="skipNavi">
-  <h1 class="blind">NS홈쇼핑 스킵네비게이션</h1>
-	<ul>
-		<li><a href="#contents" class="skipLink">본문바로가기</a></li>
-		<li><a href="#topmenu" class="skipLink">주메뉴바로가기</a></li>
-	</ul>
-</div>
-<!-- s: #doc //-->
-<div id="doc" class="isPage">
-	<!-- s:	.doc-pg //-->
-			<!-- s: header-Warp //-->
-			<?php include_once ('_Inc/headerInc.php');?>
-			<!-- e : header-wrap //-->
-			<!-- s: #container-wrap //-->
-			<div id="container-wrap"  class="div-wrap mcontainer" >
-				<div id="contents" class="div-cont">
-
-			
-
-
+    <div id="skipNavi">
+        <h1 class="blind">NS홈쇼핑 스킵네비게이션</h1>
+        <ul>
+            <li><a href="#contents" class="skipLink">본문바로가기</a></li>
+            <li><a href="#topmenu" class="skipLink">주메뉴바로가기</a></li>
+        </ul>
+    </div>
+    <!-- s: #doc //-->
+    <div id="doc" class="isPage">
+        <!-- s:	.doc-pg //-->
+        <!-- s: header-Warp //-->
+        <?php include_once ('_Inc/headerInc.php');?>
+        <!-- e : header-wrap //-->
+        <!-- s: #container-wrap //-->
+        <div id="container-wrap" class="div-wrap mcontainer">
+            <div id="contents" class="div-cont">
--- theme/nshome/head.main2.php
+++ theme/nshome/head.main2.php
@@ -30,79 +30,86 @@ header("Pragma: no-cache"); // HTTP/1.0
 ?>
 <!doctype html>
 <html lang="ko">
+
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0 ,maximum-scale=1.0, minimum-scale=1.0,user-scalable=no,target-densitydpi=medium-dpi"> 반응형용 -->
-<?php
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0 ,maximum-scale=1.0, minimum-scale=1.0,user-scalable=no,target-densitydpi=medium-dpi"> 반응형용 -->
+    <?php
 
 if($config['cf_add_meta'])
     echo $config['cf_add_meta'].PHP_EOL;
 ?>
-<title><?php echo $g5_head_title; ?></title>
-<!--[if lte IE 8]>
+    <title><?php echo $g5_head_title; ?></title>
+    <!--[if lte IE 8]>
 <script src="<?php echo G5_JS_URL ?>/html5.js"></script>
 <![endif]-->
-<script>
-// 자바스크립트에서 사용하는 전역변수 선언
-var g5_url       = "<?php echo G5_URL ?>";
-var g5_bbs_url   = "<?php echo G5_BBS_URL ?>";
-var g5_is_member = "<?php echo isset($is_member)?$is_member:''; ?>";
-var g5_is_admin  = "<?php echo isset($is_admin)?$is_admin:''; ?>";
-var g5_is_mobile = "<?php echo G5_IS_MOBILE ?>";
-var g5_bo_table  = "<?php echo isset($bo_table)?$bo_table:''; ?>";
-var g5_sca       = "<?php echo isset($sca)?$sca:''; ?>";
-var g5_editor    = "<?php echo ($config['cf_editor'] && $board['bo_use_dhtml_editor'])?$config['cf_editor']:''; ?>";
-var g5_cookie_domain = "<?php echo G5_COOKIE_DOMAIN ?>";
-</script>
-<script src="<?php echo G5_JS_URL ?>/jquery.menu.js?ver=<?php echo G5_JS_VER; ?>"></script>
-<script src="<?php echo G5_JS_URL ?>/common.js?ver=<?php echo G5_JS_VER; ?>"></script>
-<script src="<?php echo G5_JS_URL ?>/wrest.js?ver=<?php echo G5_JS_VER; ?>"></script>
-<script src="<?php echo G5_JS_URL ?>/placeholders.min.js"></script>
-<link rel="stylesheet" href="<?php echo G5_JS_URL ?>/font-awesome/css/font-awesome.min.css">
-<?php
+    <script>
+    // 자바스크립트에서 사용하는 전역변수 선언
+    var g5_url = "<?php echo G5_URL ?>";
+    var g5_bbs_url = "<?php echo G5_BBS_URL ?>";
+    var g5_is_member = "<?php echo isset($is_member)?$is_member:''; ?>";
+    var g5_is_admin = "<?php echo isset($is_admin)?$is_admin:''; ?>";
+    var g5_is_mobile = "<?php echo G5_IS_MOBILE ?>";
+    var g5_bo_table = "<?php echo isset($bo_table)?$bo_table:''; ?>";
+    var g5_sca = "<?php echo isset($sca)?$sca:''; ?>";
+    var g5_editor = "<?php echo ($config['cf_editor'] && $board['bo_use_dhtml_editor'])?$config['cf_editor']:''; ?>";
+    var g5_cookie_domain = "<?php echo G5_COOKIE_DOMAIN ?>";
+    </script>
+    <script src="<?php echo G5_JS_URL ?>/jquery.menu.js?ver=<?php echo G5_JS_VER; ?>"></script>
+    <script src="<?php echo G5_JS_URL ?>/common.js?ver=<?php echo G5_JS_VER; ?>"></script>
+    <script src="<?php echo G5_JS_URL ?>/wrest.js?ver=<?php echo G5_JS_VER; ?>"></script>
+    <script src="<?php echo G5_JS_URL ?>/placeholders.min.js"></script>
+    <link rel="stylesheet" href="<?php echo G5_JS_URL ?>/font-awesome/css/font-awesome.min.css">
+    <?php
 if(G5_IS_MOBILE) {
     echo '<script src="'.G5_JS_URL.'/modernizr.custom.70111.js"></script>'.PHP_EOL; // overflow scroll 감지
 }
 if(!defined('G5_IS_ADMIN'))
     echo $config['cf_add_script'];
 ?>
-<link type="text/css" rel="stylesheet" media="all" href="<?php echo G5_URL ?>/_Css/common.css?ver=<?php echo G5_CSS_VER; ?>" />
-<link type="text/css" rel="stylesheet" media="all" href="<?php echo G5_URL ?>/_Css/styleDefault.css?ver=<?php echo G5_CSS_VER; ?>" />
-<link type="text/css" rel="stylesheet" media="all" href="<?php echo G5_URL ?>/_Css/layout.css?ver=<?php echo G5_CSS_VER; ?>" />
-<link type="text/css" rel="stylesheet" media="all" href="<?php echo G5_URL ?>/_Css/main.css?ver=<?php echo G5_CSS_VER; ?>" />
-<link type="text/css" rel="stylesheet" media="all" href="<?php echo G5_URL ?>/static/css/common.css?v=<?php echo time(); ?>" />
+    <link type="text/css" rel="stylesheet" media="all"
+        href="<?php echo G5_URL ?>/_Css/common.css?v=<?php echo date("his")?>" />
+    <link type="text/css" rel="stylesheet" media="all"
+        href="<?php echo G5_URL ?>/_Css/styleDefault.css?v=<?php echo date("his")?>" />
+    <link type="text/css" rel="stylesheet" media="all"
+        href="<?php echo G5_URL ?>/_Css/layout.css?v=<?php echo date("his")?>" />
+    <link type="text/css" rel="stylesheet" media="all"
+        href="<?php echo G5_URL ?>/_Css/main.css?v=<?php echo date("his")?>" />
+    <link type="text/css" rel="stylesheet" media="all"
+        href="<?php echo G5_URL ?>/static/css/common.css?v=<?php echo time(); ?>" />
 
-<link rel="stylesheet" href="<?php echo G5_URL ?>/_Js/jquery/animate.css">
-<link rel="stylesheet" href="<?php echo G5_URL ?>/_Js/jquery/slick-theme.css">
-<link rel="stylesheet" href="<?php echo G5_URL ?>/_Js/jquery/slick.css">
-<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/jquery-1.11.1.min.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/jquery-1.11.3.min.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/jquery.easing.1.3.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/slick.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/owl.carousel.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/jquery.mousewheel.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/topmenu.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/defalut.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/common.js"></script>
-<script  type="text/javascript" src="<?php echo G5_URL ?>/_Js/main.js"></script>
+    <link rel="stylesheet" href="<?php echo G5_URL ?>/_Js/jquery/animate.css">
+    <link rel="stylesheet" href="<?php echo G5_URL ?>/_Js/jquery/slick-theme.css">
+    <link rel="stylesheet" href="<?php echo G5_URL ?>/_Js/jquery/slick.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/slick.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/owl.carousel.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/jquery/jquery.mousewheel.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/topmenu.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/defalut.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/common.js"></script>
+    <script type="text/javascript" src="<?php echo G5_URL ?>/_Js/main.js"></script>
 </head>
+
 <body id="body">
-<div id="skipNavi">
-  <h1 class="blind">NS홈쇼핑 스킵네비게이션</h1>
-	<ul>
-		<li><a href="#contents" class="skipLink">본문바로가기</a></li>
-		<li><a href="#topmenu" class="skipLink">주메뉴바로가기</a></li>
-	</ul>
-</div>
-<!-- s: #doc //-->
-<div id="doc" class="isPage">
-	<!-- s:	.doc-pg //-->
-	<!-- s: header-Warp //-->
-	<?php include_once (G5_PATH.'/_Inc/headerInc.php');?>
-	<!-- e : header-wrap //-->
-	<!-- s: #container-wrap //-->
-	<div id="container-wrap"  class="div-wrap mcontainer" >
-		<div id="contents" class="div-cont">
+    <div id="skipNavi">
+        <h1 class="blind">NS홈쇼핑 스킵네비게이션</h1>
+        <ul>
+            <li><a href="#contents" class="skipLink">본문바로가기</a></li>
+            <li><a href="#topmenu" class="skipLink">주메뉴바로가기</a></li>
+        </ul>
+    </div>
+    <!-- s: #doc //-->
+    <div id="doc" class="isPage">
+        <!-- s:	.doc-pg //-->
+        <!-- s: header-Warp //-->
+        <?php include_once (G5_PATH.'/_Inc/headerInc.php');?>
+        <!-- e : header-wrap //-->
+        <!-- s: #container-wrap //-->
+        <div id="container-wrap" class="div-wrap mcontainer">
+            <div id="contents" class="div-cont">
