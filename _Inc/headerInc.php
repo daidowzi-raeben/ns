b569da219f8eaaa1c0fa48842ec4582dd2d54308
장영욱 <raeben@naver.com>
2024-12-05 11:41:10 +0900

    fix

--- _Inc/headerInc.php
+++ _Inc/headerInc.php
@@ -283,33 +283,39 @@
 		
 		var ret = window.open("/temp/index.html", "popwin", "status=0, width=680, height=722, scrollbars=1, left=" + popupX);
 	}
+
+  	function movieWin()
+	{
+		var popupX = (document.body.offsetWidth / 2) - 300;
+		
+		var ret = window.open("/temp/movie.html", "popwin", "status=0, width=680, height=500, scrollbars=1, left=" + popupX);
+	}
 	
-	// testWin();
 </script>
+<!-- sejong~5273 -->
 <?php
 	//
 	//echo G5_TIME_YMDHIS;
-	$tmpStartTime = '2024-04-25 00:00:00';
-	$tmpEndTime = '2024-05-17 18:01:00';
+  
+	$tmpStartTime = '2024-11-09 00:00:00';
 
-  if($member['mb_id'] === 'admin') {
-    $tmpStartTime = '2024-04-22 00:00:00';
-  }
-
-
+	$tmpEndTime = '2024-12-13 18:01:00';
 	
 	//로그인 체크
 	if($is_member) {
+    ?>
+<script type='text/javascript'>movieWin();</script>
+    <?php
 		// 서약서팝업레이어 시작 { : 로그인 회원인 경우만 서약서 출력
 		if(G5_TIME_YMDHIS > $tmpStartTime && G5_TIME_YMDHIS < $tmpEndTime)
 		{
-			$sql = " select count(pld_no) as cnt from sj_prs_pledge where mb_id = '{$member['mb_id']}' and pld_year = '2024' and pld_semi = 'A' ";
+			$sql = " select count(pld_no) as cnt from sj_prs_pledge where mb_id = '{$member['mb_id']}' and pld_year = '2024' and pld_semi = 'B' ";
 			$result = sql_fetch($sql);
 			
 			if(!$result['cnt']) 
 			{
 ?>
-				<script type='text/javascript'>testWin();</script> 
+				<!-- <script type='text/javascript'>testWin();</script>  -->
 <?php
 			}
 		}
