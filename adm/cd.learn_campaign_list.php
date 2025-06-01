4a5de094c4e66ff3f2d6583516d81112c3353141
raeben <45227695+daidowzi-raeben@users.noreply.github.com>
2025-06-01 16:38:46 +0900

    fix

--- adm/cd.learn_campaign_list.php
+++ adm/cd.learn_campaign_list.php
@@ -87,7 +87,7 @@ $colspan = $result_cnt['cnt'];
 				<th scope="col" id="mb_list_id" rowspan="2">이름</th>
 				<th scope="col" id="mb_list_id" rowspan="2">아이디</th>
 				<th scope="col" id="mb_list_id" rowspan="2">학습시작일</th>
-				<th scope="col" id="mb_list_id" rowspan="2">학습종료일일</th>
+				<th scope="col" id="mb_list_id" rowspan="2">학습종료일</th>
 				<th scope="col" id="mb_list_name" colspan="<?php echo $colspan - 1?>">항목별 수행결과</th>
 <!-- 				<th scope="col" id="mb_list_id" rowspan="2">마일리지 합계</th> -->
 			</tr>
@@ -115,16 +115,11 @@ $colspan = $result_cnt['cnt'];
 				$mb_3 = $row['mb_3'];
 				$mb_point = $row['mb_point'];
 				
-				echo $row['cpt_contents']."??";
 				$lssn_data_new = sql_fetch("SELECT * from sj_lms_chapter_attend 
 				where app_uid = '{$mb_id}' and att_lssn_no = '33' and att_contents = '{$row['cpt_contents']}'
 				 ");
 
-				 echo "SELECT * from sj_lms_chapter_attend 
-				where app_uid = '{$mb_id}' and att_lssn_no = '33' and att_contents = '{$row['cpt_contents']}'
-				 ";
-				
-				
+
 
 				$sql_list = "SELECT * from sj_lms_adm_list ORDER BY idx asc";
 				$result_list = sql_query($sql_list);
@@ -202,13 +197,14 @@ $colspan = $result_cnt['cnt'];
 				<td headers="cb_list_"><?php echo $lssn_data_new['att_rdate'] ?></td>
 				<td headers="cb_list_"><?php echo $lssn_data_new['att_study_last']?></td>
 				<?php 
-				$sql_list = "SELECT * from sj_lms_lesson WHERE lssn_kind = '".$L."'  ORDER BY lssn_rdate asc";
+				$sql_list = "SELECT * from sj_lms_chapter WHERE cpt_lesson = '33'";
 				$result_list = sql_query($sql_list);
 				for ($u=0; $row3=sql_fetch_array($result_list); $u++) { 
-					$sql_is = " SELECT * from cd_lms_lesson_result WHERE mb_id = '".$mb_id."' AND lssn_no = '".$row3['lssn_no']."' limit 1 ";
+					$sql_is = " SELECT * from sj_lms_chapter_attend 
+				where app_uid = '{$mb_id}' and att_lssn_no = '33' and att_contents = '{$row['cpt_contents']}' limit 1";
 					$result_is = sql_fetch($sql_is);
 					?>
-				<td headers="cb_list_"><?php if(isset($result_is['mb_id'])) {echo '1'; }else {echo '0';}?></td>
+				<td headers="cb_list_"><?php if(isset($result_is['att_study_rate'])) {echo $result_is['att_study_rate']; }else {echo '0';}?></td>
 				<?php }?>
 <!-- 				<td headers="cb_list_"><?php echo $str_cyber4 ?></td>
 				<td headers="cb_list_"><?php echo $str_cyber40 ?></td>
