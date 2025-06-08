adea31f079dacd4e6543490863b7ab1c5f5cc6ae
raeben <45227695+daidowzi-raeben@users.noreply.github.com>
2025-06-08 22:08:45 +0900

    fix

--- test.php
+++ test.php
@@ -1,4 +1,8 @@
 <?php
-	$study_rate = ceil( 1 / 3 * 100 );
-	echo $study_rate;
+$link = mysql_connect('175.126.82.119', 'root', 'Rlxk5273');
+if (!$link) {
+    die('MySQL 접속 실패: ' . mysql_error());
+}
+echo 'MySQL 접속 성공!';
+mysql_close($link);
 ?>
