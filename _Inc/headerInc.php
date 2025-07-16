b1f5274ba8d301a1cea30776301d5c36678b52c2
raeben <45227695+daidowzi-raeben@users.noreply.github.com>
2025-07-16 12:55:06 +0900

    fix

--- _Inc/headerInc.php
+++ _Inc/headerInc.php
@@ -14,7 +14,7 @@
 	if(!$res['cnt'] && $member['mb_id']) 
 	{
   ?>
-  <a  target="_blank" href="<?php echo G5_URL ?>/bbs/board.php?bo_table=notice" class="quick-menu">
+  <a  target="_blank" href="<?php echo G5_URL ?>/survey/form/survey4.html" class="quick-menu">
     
     CP인식도<br>조사
   </a>
@@ -27,7 +27,7 @@
 	if(!$res['cnt'] && $member['mb_id']) 
 	{
   ?>
-  <a target="_blank" href="<?php echo G5_URL ?>/bbs/board.php?bo_table=notice" class="quick-menu">
+  <a target="_blank" href="<?php echo G5_URL ?>/survey/form/survey1.html" class="quick-menu">
     
     CP교육<br>만족도 조사
   </a>
