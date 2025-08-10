7b9e3844a6e7990741c853d899a18f2d27f19bf9
raeben <45227695+daidowzi-raeben@users.noreply.github.com>
2025-08-10 18:29:10 +0900

    fix

--- skin/board/ns/view.skin.php
+++ skin/board/ns/view.skin.php
@@ -113,18 +113,18 @@ if($bo_table == "notice") {
 
 				  </tbody>
 			</table>
-			<div class="ssgap"></div>
 			<div>
 				    <?php 
     if($member['mb_id'] == 'admin') {
         ?>
-        
+        <div id="contents">
         <div class="confirm">
           <a href="javascript:confirmProc()">확인</a><span>페이지 다 읽으신 후 <strong>확인버튼</strong>을 클릭해 주시면 나의 마일리지에 반영됩니다.</span>
         </div>
-    
+    </div>
     <?php } ?>
 			</div>
+			<div class="ssgap"></div>
 			<div class="r">
 				<a href="<?php echo $list_href ?>" class="bw-btn"><span>목록</span></a>
 			</div>
