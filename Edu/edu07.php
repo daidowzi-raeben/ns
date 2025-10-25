e7883646c39abcdee7388f7013524c9318bbdf49
장영욱 <raeben@naver.com>
2025-10-25 16:03:48 +0900

    fix

--- Edu/edu07.php
+++ Edu/edu07.php
@@ -120,6 +120,9 @@ $result_m = sql_fetch($sql_m);
 $sql_m2 = "SELECT *  FROM sj_lesson_apply where app_uid = '{$member['mb_id']}' and app_lssn_no = '33' limit 0, 1 ";
 $result_m2 = sql_fetch($sql_m2);
 
+$sql_m3 = "SELECT *  FROM sj_lesson_apply where app_uid = '{$member['mb_id']}' and app_lssn_no = '34' limit 0, 1 ";
+$result_m3 = sql_fetch($sql_m3);
+
 
 if (!$sst) {
     $sst = "ls.lssn_no";
@@ -291,8 +294,8 @@ $result = sql_query($sql);
                             {
                     ?>
                     <a href="/Edu/class21.php?ls=34" class="class-enter"
-                        style="<?php if (	$result_m2['app_study_rate'] == 100) { echo "background: #5a5ae7;"; }?>"><span>
-                            <?php if (	$result_m2['app_study_rate'] == 100) {
+                        style="<?php if (	$result_m3['app_study_rate'] == 100) { echo "background: #5a5ae7;"; }?>"><span>
+                            <?php if (	$result_m3['app_study_rate'] == 100) {
                                 echo '학습완료';
                             } else  {
                                 echo '학습하기';
