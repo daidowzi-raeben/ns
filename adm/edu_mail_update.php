bf66a32b624b49dd1948c7d786a3ce5386f92542
장영욱 <raeben@naver.com>
2026-07-01 21:47:39 +0900

    fix

--- adm/edu_mail_update.php
+++ adm/edu_mail_update.php
@@ -15,7 +15,9 @@ $emq_reserve_time = $_POST['emq_reserve_time'];
 $emq_use_unsubscribe = (int)$_POST['emq_use_unsubscribe'];
 $emq_target_ids = trim($_POST['emq_target_ids']);
 
-if (!$emq_subject || !$emq_content || !$emq_target_lesson || !$emq_reserve_time) {
+$lesson_check = ($emq_target_type === 'cp') ? true : ($emq_target_lesson > 0);
+
+if (!$emq_subject || !$emq_content || !$lesson_check || !$emq_reserve_time) {
     alert('모든 필수 항목을 입력해 주세요.');
 }
 
